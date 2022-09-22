d1b0a88b56624ce204bc6a0278757670b27699c1
chloeehulme <89981993+chloeehulme@users.noreply.github.com>
2022-09-22 19:24:09 +1000

    Add files via upload

--- DB Scripts/art-gallery-dbscript.sql
+++ DB Scripts/art-gallery-dbscript.sql
@@ -1,33 +1,28 @@
 CREATE DATABASE ARTGALLERY;
 
 CREATE TABLE public.state (
-    state_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50) NOT NULL,
-    num_sub_groups INT NOT NULL,
-    num_languages INT NOT NULL,
+    language_groups INT NOT NULL,
     created_date TIMESTAMP NOT NULL,
     modified_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (state_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE public.artist (
-    artist_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     description VARCHAR(800) NULL,
     age INT NULL,
     state_id INT NOT NULL,
-    language VARCHAR(100) NOT NULL,
-    sub_group VARCHAR(100) NOT NULL,
+    language_group VARCHAR(100) NOT NULL,
     created_date TIMESTAMP NOT NULL,
     modified_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (artist_id),
-    CONSTRAINT fk_state
-        FOREIGN KEY (state_id)
-            REFERENCES public.state(state_id)
+    PRIMARY KEY (id),
 );
 
 CREATE TABLE public.artefact (
-    artefact_id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
     artist_id INT NOT NULL,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(800) NULL,
@@ -38,8 +33,5 @@ CREATE TABLE public.artefact (
     img_url VARCHAR(100) NOT NULL,
     created_date TIMESTAMP NOT NULL,
     modified_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (artefact_id),
-    CONSTRAINT fk_artist
-        FOREIGN KEY (artist_id)
-            REFERENCES public.artist(artist_id)
+    PRIMARY KEY (id)
 );
